d3d971be8ccd17b370b739aa69e985cd49713e2c
Rohan <rohanpschauhan@gmail.com>
2024-11-02 18:14:29 +0000

    created main.py and added add, show and exit functionality for todo app

---
+++ Version/Command_Line_App/main.py
@@ -0,0 +1,14 @@
+todos = []
+
+while True:
+    user_input = input("What do you want to do? (Type 'add', 'show' or exit):  ")
+    match user_input:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show':
+            print(todos)
+        case 'exit':
+            break
+print("Thank you and have a good day! Bye!")
+    
