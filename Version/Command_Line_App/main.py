3c9b10802e436773b7e54f5a0fb8cb2a0659ce7e
Rohan <rohanpschauhan@gmail.com>
2024-11-03 12:39:30 +0000

    created a complete functionality where user can 'complete' a task and delete it from list

--- Version/Command_Line_App/main.py
+++ Version/Command_Line_App/main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_input = input("What do you want to do? (Type 'add', 'show', 'edit' or exit):  ")
+    user_input = input("What do you want to do? (Type 'add', 'show', 'edit', 'complete' or exit):  ")
     user_input = user_input.strip()
     
     match user_input:
@@ -10,16 +10,19 @@ while True:
             todos.append(todo)
         case 'show':
             for index, item in enumerate(todos):
-                row = f"{index} - {item}"
+                row = f"{index + 1} - {item}"
                 print(row)
         case 'edit':
             number = int(input("Number of the todo to edit: "))
             number = number - 1
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo
+        case 'complete':
+            number = int(input("Number of the todo to complete: "))
+            todos.pop(number - 1)
         case 'exit':
             break
         case x:
-            print("Sorry, please enter either 'add' to add to your list, 'show' to show the list so far, 'edit' to edit the list or 'exit' to exit")
+            print("Sorry, please enter either 'add' to add to your list, 'show' to show the list so far, 'edit' to edit the list, 'complete' to remove an item from the list. Alternatively, type 'exit' to exit the application")
 print("Thank you and have a good day! Bye!")
     
